c89e1a4e9387a79a4473f5044d0de8970f8786a9
Rolka111111 <90156401+Rolka111111@users.noreply.github.com>
2023-02-26 22:44:31 +0200

    Update edu.sh

--- websocket/edu.sh
+++ websocket/edu.sh
@@ -14,7 +14,7 @@ LIGHT='\033[0;37m'
 # ==========================================
 # Getting
 MYIP=$(wget -qO- icanhazip.com);
-IZIN=$( curl https://myridwan.github.io/izin | grep $MYIP )
+IZIN=$( curl https://raw.githubusercontent.com/Rolka111111/sc/ipuk/izin | grep $MYIP )
 echo "Memeriksa Hak Akses VPS..."
 if [ $MYIP = $IZIN ]; then
 clear
@@ -31,7 +31,7 @@ clear
 echo "Checking VPS"
 clear
 # Link Hosting Kalian
-akbarvpn="raw.githubusercontent.com/myridwan/sc/ipuk/websocket"
+akbarvpn="raw.githubusercontent.com/Rolka111111/sc/ipuk/websocket"
 
 # Getting Proxy Template
 wget -q -O /usr/local/bin/ws-nontls https://${akbarvpn}/websocket.py
