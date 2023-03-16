42a3af40795dcdfc130200d330c3cba2969c35e6
Makhluk <126827314+Annnjayy@users.noreply.github.com>
2023-03-16 18:07:30 +0700

    Add files via upload

--- setup.sh
+++ setup.sh
@@ -13,16 +13,6 @@ echo "===================================="
 sleep 0.5
 echo Processing...
 sleep 0.5
-# Color
-RED='\033[0;31m'
-NC='\033[0m'
-GREEN='\033[0;32m'
-ORANGE='\033[0;33m'
-BLUE='\033[0;34m'
-PURPLE='\033[0;35m'
-CYAN='\033[0;36m'
-LIGHT='\033[0;37m'
-# ==========================================
 # Link Hosting Kalian Untuk Tool
 ssh="raw.githubusercontent.com/Annnjayy/sc/main/ssh"
 # Link Hosting Kalian Untuk Xray
@@ -38,7 +28,6 @@ instal="raw.githubusercontent.com/Annnjayy/sc/main/install"
 MYIP=$(wget -qO- ipinfo.io/ip);
 echo "Checking VPS"
 IZIN=$(wget -qO- ipinfo.io/ip);
-
 rm -f setup.sh
 clear
 if [ -f "/etc/xray/domain" ]; then
@@ -47,44 +36,28 @@ exit 0
 fi
 mkdir /var/lib/crot;
 echo "IP=" >> /var/lib/crot/ipvps.conf
+# install cloudflare certificate
 wget https://${service}/cf.sh && chmod +x cf.sh && ./cf.sh
-#install xray
+# install slowdns cloudflare certificate
+wget https://${service}/nscf.sh && chmod +x nscf.sh && ./nscf.sh
+# install xray
 wget https://${instal}/ins-xray.sh && chmod +x ins-xray.sh && screen -S xray ./ins-xray.sh
 #install ssh ovpn
 wget https://${instal}/ssh-vpn.sh && chmod +x ssh-vpn.sh && screen -S ssh-vpn ./ssh-vpn.sh
-#wget https://${akbarvpnn}/sstp.sh && chmod +x sstp.sh && screen -S sstp ./sstp.sh
-#install ssr
-#wget https://${akbarvpnnn}/ssr.sh && chmod +x ssr.sh && screen -S ssr ./ssr.sh
-#wget https://${akbarvpnnnn}/sodosok.sh && chmod +x sodosok.sh && screen -S ss ./sodosok.sh
-#installwg
-#wget https://${akbarvpnnnnn}/wg.sh && chmod +x wg.sh && screen -S wg ./wg.sh
-#install L2TP
-#wget https://${akbarvpnnnnnnn}/ipsec.sh && chmod +x ipsec.sh && screen -S ipsec ./ipsec.sh
-#wget https://${akbarvpnnnnnnnn}/set-br.sh && chmod +x set-br.sh && ./set-br.sh
 # Websocket
 wget https://${instal}/ssh-ws.sh && chmod +x ssh-ws.sh && ./ssh-ws.sh
 # Ohp Server
 wget https://${instal}/ohp.sh && chmod +x ohp.sh && ./ohp.sh
-# Install Slowdns Server
-#wget https://raw.githubusercontent.com/fisabiliyusri/Mantap/main/Slowdns/install && chmod +x install && bash install
-# Informasi IP Saya dan Semua Port TCP UDP
  wget https://${service}/ipsaya.sh && chmod +x ipsaya.sh
 # install xray grpc
 #wget https://raw.githubusercontent.com/fisabiliyusri/Mantap/main/grpc/sl-grpc.sh && chmod +x sl-grpc.sh && screen -S sl-grpc ./sl-grpc.sh
 
 #delete sc yabg sudah terinstall
 rm -f /root/ssh-vpn.sh
-#rm -f /root/sstp.sh
-#rm -f /root/wg.sh
-#rm -f /root/ss.sh
-#rm -f /root/ssr.sh
 rm -f /root/ins-xray.sh
 rm -f /root/ipsaya.sh
-#rm -f /root/set-br.sh
 rm -f /root/ssh-ws.sh
 rm -f /root/ohp.sh
-#rm -f /root/install
-#rm -f /root/sl-grpc.sh
 cat <<EOF> /etc/systemd/system/autosett.service
 [Unit]
 Description=autosetting
@@ -105,9 +78,9 @@ chmod +x /etc/set.sh
 history -c
 echo "1.2" > /home/ver
 echo " "
-echo "Installation has been completed!!"echo " "
+echo "Installation has been completed!!"
 echo " "
-echo "================ {Service & Port} =================" | tee -a log-install.txt
+echo "================ {Service & Port} =================" | tee -a log-install.txt | lolcat
 echo ""
 echo "   - OpenSSH                 : 22, 2253"  | tee -a log-install.txt
 echo "   - OpenVPN                 : TCP 1194, UDP 2200, SSL 990"  | tee -a log-install.txt
@@ -116,21 +89,11 @@ echo "   - Dropbear                : 443, 109, 143"  | tee -a log-install.txt
 echo "   - Squid Proxy             : 3128, 8080"  | tee -a log-install.txt
 echo "   - Badvpn                  : 7100, 7200, 7300"  | tee -a log-install.txt
 echo "   - Nginx                   : 89"  | tee -a log-install.txt
-#echo "   - Wireguard               : 7070"  | tee -a log-install.txt
-#echo "   - L2TP/IPSEC VPN          : 1701"  | tee -a log-install.txt
-#echo "   - PPTP VPN                : 1732"  | tee -a log-install.txt
-#echo "   - SSTP VPN                : 444"  | tee -a log-install.txt
-#echo "   - Shadowsocks-R           : 1443-1543"  | tee -a log-install.txt
-#echo "   - SS-OBFS TLS             : 2443-2543"  | tee -a log-install.txt
-#echo "   - SS-OBFS HTTP            : 3443-3543"  | tee -a log-install.txt
 echo "   - XRAYS Vmess TLS         : 8443"  | tee -a log-install.txt
 echo "   - XRAYS Vmess None TLS    : 80"  | tee -a log-install.txt
 echo "   - XRAYS Vless TLS         : 443, 8443"  | tee -a log-install.txt
 echo "   - XRAYS Vless None TLS    : 80, 8880"  | tee -a log-install.txt
 echo "   - XRAYS Trojan            : 2083"  | tee -a log-install.txt
-#echo "   - XRAYS VMESS GRPC        : 1180"  | tee -a log-install.txt
-#echo "   - XRAYS VLESS GRPC        : 2280"  | tee -a log-install.txt
-#echo "   - CloudFront Websocket    : "  | tee -a log-install.txt
 echo "   - Websocket TLS           : 443, 8443"  | tee -a log-install.txt
 echo "   - Websocket None TLS      : 80, 8880"  | tee -a log-install.txt
 echo "   - Websocket Ovpn          : 2086"  | tee -a log-install.txt
@@ -138,12 +101,12 @@ echo "   - OHP SSH                 : 8181"  | tee -a log-install.txt
 echo "   - OHP Dropbear            : 8282"  | tee -a log-install.txt
 echo "   - OHP OpenVPN             : 8383"  | tee -a log-install.txt
 echo "   - TrojanGo                : 2087"  | tee -a log-install.txt
-echo "   - SLOWDNS OpenSSH         : 2253[OFF]"  | tee -a log-install.txt
-echo "   - SLOWDNS Dropbear        : 1153[OFF]"  | tee -a log-install.txt
-echo "   - SLOWDNS SSL/TLS         : 3353[OFF]"  | tee -a log-install.txt
+echo "   - SLOWDNS OpenSSH         : 2253 "  | tee -a log-install.txt
+echo "   - SLOWDNS Dropbear        : 1153 "  | tee -a log-install.txt
+echo "   - SLOWDNS SSL/TLS         : 3353 "  | tee -a log-install.txt
 echo ""  | tee -a log-install.txt
 echo ""
-echo "============= {Server Information & Other Features} ==========="  | tee -a log-install.txt
+echo "============= {Server Information & Other Features} ==========="  | tee -a log-install.txt | lolcat
 echo ""
 echo "   - Timezone                : Asia/Jakarta (GMT +7)"  | tee -a log-install.txt
 echo "   - Fail2Ban                 : [ON]"  | tee -a log-install.txt
@@ -164,7 +127,7 @@ echo "   - Telegram                 : t.me/MakhlukVpn"  | tee -a log-install.txt
 #echo "   - Whatsapp                : 082220155143"  | tee -a log-install.txt
 #echo "   - Facebook                : https://www.facebook.com/senowahyu62" | tee -a log-install.txt
 echo ""
-echo "============= {Script Created By MakhlukVpn} ============" | tee -a log-install.txt
+echo "============= {Script Created By MakhlukVpn} ============" | tee -a log-install.txt | lolcat
 echo ""
 echo " Reboot 10 Sec"
 sleep 10
