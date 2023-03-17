52bc55ef0fe2a7ed3812567c2ec9e121c1fee7b3
Makhluk <126827314+Annnjayy@users.noreply.github.com>
2023-03-18 03:43:03 +0700

    Add files via upload

--- ssh/autokill.sh
+++ ssh/autokill.sh
@@ -15,11 +15,11 @@ else
 sts="${Error}"
 fi
 clear
-echo -e ""
+echo -e " "
 echo -e "===============================" | lolcat
 echo -e "      Status Autokill $sts            "
 echo -e "===============================" | lolcat
-echo -e ""
+echo -e " "
 echo -e " [1]   AutoKill After 5 Minutes"
 echo -e " [2]   AutoKill After 10 Minutes"
 echo -e " [3]   AutoKill After 15 Minutes"
@@ -28,63 +28,63 @@ echo -e " [x]   Exit"
 echo -e "===============================" | lolcat
 echo -e "      Script By MakhlukVpn         "
 echo -e "===============================" | lolcat
-echo -e ""
-echo -e ""
+echo -e " "
+echo -e " "
 echo -e "===============================" | lolcat
 read -p "   Select From Options [1-4 or x] :  " AutoKill
 read -p "   Multilogin Maximum Number  : " max
 echo -e "===============================" | lolcat
-echo -e ""
+echo -e " "
 case $AutoKill in
 1)
-echo -e ""
+echo -e " "
 sleep 1
 clear
 echo > /etc/cron.d/tendang
 echo "# Autokill" >>/etc/cron.d/tendang
 echo "*/5 * * * *  root /usr/bin/tendang $max" >>/etc/cron.d/tendang
-echo -e ""
+echo -e " "
 echo -e "===============================" | lolcat
-echo -e ""
+echo -e " "
 echo -e "    Allowed MultiLogin : $max"
 echo -e "    AutoKill Every     : 5 Minutes"
-echo -e ""
+echo -e " "
 echo -e "===============================" | lolcat
 echo -e "     Script By MakhlukVpn         "
 echo -e "===============================" | lolcat
 exit  
 [ $MYIP = $MYIP ];;
 2)
-echo -e ""
+echo -e " "
 sleep 1
 clear
 echo > /etc/cron.d/tendang
 echo "# Autokill" >>/etc/cron.d/tendang
 echo "*/10 * * * *  root /usr/bin/tendang $max" >>/etc/cron.d/tendang
-echo -e ""
+echo -e " "
 echo -e "===============================" | lolcat
-echo -e ""
+echo -e " "
 echo -e "    Allowed MultiLogin : $max"
 echo -e "    AutoKill Every     : 10 Minutes"
-echo -e ""
+echo -e " "
 echo -e "===============================" | lolcat
 echo -e "     Script By MakhlukVpn         "
 echo -e "===============================" | lolcat
 exit
 [ $MYIP = $MYIP ];;
 3)
-echo -e ""
+echo -e " "
 sleep 1
 clear
 echo > /etc/cron.d/tendang
 echo "# Autokill" >>/etc/cron.d/tendang
 echo "*/15 * * * *  root /usr/bin/tendang $max" >>/etc/cron.d/tendang
-echo -e ""
+echo -e " "
 echo -e "===============================" | lolcat
-echo -e ""
+echo -e " "
 echo -e "    Allowed MultiLogin : $max"
 echo -e "    AutoKill Every     : 15 Minutes"
-echo -e ""
+echo -e " "
 echo -e "===============================" | lolcat
 echo -e "     Script By MakhlukVpn         "
 echo -e "===============================" | lolcat
@@ -95,9 +95,9 @@ clear
 echo > /etc/cron.d/tendang
 echo -e ""
 echo -e "===============================" | lolcat
-echo -e ""
+echo -e " "
 echo -e "    AutoKill MultiLogin Turned Off  "
-echo -e ""
+echo -e " "
 echo -e "===============================" | lolcat
 echo -e "     Script By MakhlukVpn         "
 echo -e "===============================" | lolcat
