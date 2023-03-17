3dc7dbd626efa00054e90d0c40de0eb6af9f49c9
Makhluk <126827314+Annnjayy@users.noreply.github.com>
2023-03-18 03:44:08 +0700

    Add files via upload

--- xray/addtrojan.sh
+++ xray/addtrojan.sh
@@ -15,53 +15,47 @@ LIGHT='\033[0;37m'
 MYIP=$(wget -qO- ipinfo.io/ip);
 echo "Checking VPS"
 IZIN=$( curl ipinfo.io/ip | grep $MYIP )
-if [ $MYIP = $MYIP ]; then
-echo -e "${NC}${GREEN}Permission Accepted...${NC}"
-else
-echo -e "${NC}${RED}Permission Denied!${NC}";
-echo -e "${NC}${LIGHT}Fuck You!!"
-exit 0
-fi
-clear
-source /var/lib/crot/ipvps.conf
+# add trojan
+uuid=$(cat /etc/trojan-go/uuid.txt)
+source /var/lib/akbarstorevpn/ipvps.conf
 if [[ "$IP" = "" ]]; then
 domain=$(cat /etc/xray/domain)
 else
 domain=$IP
 fi
-tr="$(cat ~/log-install.txt | grep -w "Trojan" | cut -d: -f2|sed 's/ //g')"
+trgo="$(cat ~/log-install.txt | grep -w "Tr Go" | cut -d: -f2|sed 's/ //g')"
 until [[ $user =~ ^[a-zA-Z0-9_]+$ && ${user_EXISTS} == '0' ]]; do
-echo -e "===============================" | lolcat
-read -rp "Password : " -e user
-user_EXISTS=$(grep -w $user /etc/xray/config.json | wc -l)
+		echo -e "===============================" | lolcat
+		read -rp "Password : " -e user
+		user_EXISTS=$(grep -w $user /etc/trojan-go/akun.conf | wc -l)
 
-if [[ ${user_EXISTS} == '1' ]]; then
-echo -e "===============================" | lolcat
-echo -e "  Username ${user} Already On VPS "
-echo -e "     Please Choose Another"
-echo -e "===============================" | lolcat
-exit 1
-fi
-done
+		if [[ ${user_EXISTS} == '1' ]]; then
+			echo -e "===============================" | lolcat
+            echo -e "  Username ${user} Already On VPS "
+            echo -e "     Please Choose Another"
+            echo -e "===============================" | lolcat
+		fi
+	done
 read -p "Expired (Days) : " masaaktif
 echo -e "===============================" | lolcat
-hariini=`date -d "0 days" +"%Y-%m-%d"`
+sed -i '/"'""$uuid""'"$/a\,"'""$user""'"' /etc/trojan-go/config.json
 exp=`date -d "$masaaktif days" +"%Y-%m-%d"`
-sed -i '/#xray-trojan$/a\#&# '"$user $exp"'\
-},{"password": "'""$user""'","email": "'""$user""'"' /etc/xray/config.json
-trojanlink="trojan://${user}@${domain}:2087"
+hariini=`date -d "0 days" +"%Y-%m-%d"`
+echo -e "### $user $exp" >> /etc/trojan-go/akun.conf
+systemctl restart trojan-go.service
+link="trojan-go://${user}@${domain}:2083/?sni=${domain}&type=ws&host=${domain}&path=/trojango&encryption=none#$user"
 clear
 echo -e ""
 echo -e "========={XRAYS/TROJAN}=========" | lolcat
 echo -e "Remarks  : ${user}"
 echo -e "IP/Host  : ${MYIP}"
 echo -e "Address  : ${domain}"
-echo -e "Port     : 2087"
+echo -e "Port     : 2083, 2087"
 echo -e "Key      : ${user}"
 echo -e "Created  : $hariini"
 echo -e "Expired  : $exp"
 echo -e "===============================" | lolcat
-echo -e " Link TR  : ${trojanlink}"
+echo -e " Link TR  : ${link}"
 echo -e "===============================" | lolcat
 echo -e "   Script By MakhlukVpn          "
 echo -e "===============================" | lolcat
@@ -70,4 +64,4 @@ echo -e "       Terimakasih Telah "
 echo -e "   Menggunakan Layanan Kami "
 echo -e ""
 echo -e "===============================" | lolcat
-echo -e "
+echo -e ""
