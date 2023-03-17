89dcfc989ce40700cf8ac49880da5e4fb299636d
Makhluk <126827314+Annnjayy@users.noreply.github.com>
2023-03-18 03:41:04 +0700

    Add files via upload

--- menu/menu.sh
+++ menu/menu.sh
@@ -2,10 +2,50 @@
 clear
 red='\e[1;31m'
 green='\e[0;32m'
-clear
+# isp
+NAMAISP=$( curl -s ipinfo.io/org | cut -d " " -f 2-10  )
+COUNTRY=$( curl -s ipinfo.io/country )
+Name="$(cat /etc/xray/pengguna)"
+# os
+source /etc/os-release
+Versi_OS=$VERSION
+ver=$VERSION_ID
+Tipe=$NAME
+URL_SUPPORT=$HOME_URL
+basedong=$ID
+# Getting CPU Information
+cpu_usage1="$(ps aux | awk 'BEGIN {sum=0} {sum+=$3}; END {print sum}')"
+cpu_usage="$((${cpu_usage1/\.*} / ${corediilik:-1}))"
+cpu_usage+=" %"
+# Total Ram
+total_ram=` grep "MemTotal: " /proc/meminfo | awk '{ print $2}'`
+totalram=$(($total_ram/1024))
+# Ram Usage
+total_r2am=` grep "MemAvailable: " /proc/meminfo | awk '{ print $2}'`
+MEMORY=$(($total_r2am/1024))
+# Getting Domain Name
+MYIP=$(wget -qO- icanhazip.com);
+Domen="$(cat /etc/xray/domain)"
+# Download
+download=`grep -e "lo:" -e "wlan0:" -e "eth0" /proc/net/dev  | awk '{print $2}' | paste -sd+ - | bc`
+downloadsize=$(($download/1073741824))
+# Upload
+upload=`grep -e "lo:" -e "wlan0:" -e "eth0" /proc/net/dev | awk '{print $10}' | paste -sd+ - | bc`
+uploadsize=$(($upload/1073741824))
+
 echo -e ""
 echo -e "===============================" | lolcat
-echo -e "             Menu Manager  "        
+echo -e "     ⇱ Sytem Information ⇲           "
+echo -e "===============================" | lolcat
+echo -e "# OS Name       : $Tipe , $Versi_OS"
+echo -e "# Cpu Usage     :$cpu_usage"
+echo -e "# Total RAM     : ${totalram}MB"
+echo -e "# Avaible          : ${MEMORY}MB"
+echo -e "# Public IP        : $MYIP"
+echo -e "# Domain         : $Domen"
+echo -e "# ISP Country   : $NAMAISP , $COUNTRY"
+echo -e "===============================" | lolcat
+echo -e "     Menu Manager  "        
 echo -e "===============================" | lolcat
 echo -e ""
 echo -e " 1}.$green  Manager SSH & OpenVPN"
@@ -20,13 +60,19 @@ echo -e " 9}.$green  Cloud Manager Drive "
 echo -e " 10}.$green  Reboot Vps "
 echo -e " x}.$red  Exit"
 echo -e ""
+echo -e "=====================================" | lolcat
+echo -e "# Total Download    : $downloadsize GB "
+echo -e "# Total Upload      : $uploadsize GB "
+echo -e "# Client Name.      : $Name"
+echo -e "# Exp Script        : $Exp"
+echo -e "# Version          : V.1 "
 echo -e "===============================" | lolcat
-echo -e "        Script By MakhlukVpn          "
+echo -e "   Script By MakhlukVpn          "
 echo -e "===============================" | lolcat
 echo -e ""
 echo -e ""
 echo -e "===============================" | lolcat
-read -p "   Please Input Number  [1-10 or [x] ] :  "  menu
+read -p "  Please Input Number  [1-10 or [x] ] :  "  menu
 echo -e "===============================" | lolcat
 case $menu in
 1)
