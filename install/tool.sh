2c58dcb9118995111f9ceed15f519d39abd40d88
Makhluk <126827314+Annnjayy@users.noreply.github.com>
2023-03-19 01:01:18 +0700

    Add files via upload

--- install/tool.sh
+++ install/tool.sh
@@ -10,6 +10,24 @@ service="raw.githubusercontent.com/Annnjayy/sc/main/service"
 menu="raw.githubusercontent.com/Annnjayy/sc/main/menu"
 #Link Hosting Kalian Untuk Install
 instal="raw.githubusercontent.com/Annnjayy/sc/main/install"
+# install webserver
+apt -y install nginx php php-fpm php-cli php-mysql libxml-parser-perl
+rm /etc/nginx/sites-enabled/default
+rm /etc/nginx/sites-available/default
+curl https://${service}/nginx.conf > /etc/nginx/nginx.conf
+curl https://${service}/vps.conf > /etc/nginx/conf.d/vps.conf
+sed -i 's/listen = \/var\/run\/php-fpm.sock/listen = 127.0.0.1:9000/g' /etc/php/fpm/pool.d/www.conf
+useradd -m vps;
+mkdir -p /home/vps/public_html
+echo "<?php phpinfo() ?>" > /home/vps/public_html/info.php
+chown -R www-data:www-data /home/vps/public_html
+chmod -R g+rw /home/vps/public_html
+cd /home/vps/public_html
+wget --output-document/home/vps/public_html/index.html "https://${service}/index.html1"
+/etc/init.d/nginx restart
+cd
+chown -R www-data:www-data /home/vps/public_html
+/etc/init.d/nginx restart
 #link
 cd /usr/bin
 wget --output-document=addhost "https://${menu}/addhost.sh"
