264e1a54dcbfdc67827e0d837ee02137bb827e53
Makhluk <126827314+Annnjayy@users.noreply.github.com>
2023-03-28 02:48:13 +0700

    Add files via upload

--- service/cf.sh
+++ service/cf.sh
@@ -44,7 +44,6 @@ RESULT=$(curl -sLX PUT "https://api.cloudflare.com/client/v4/zones/${ZONE}/dns_r
      --data '{"type":"A","name":"'${SUB_DOMAIN}'","content":"'${IP}'","ttl":120,"proxied":false}')
 echo "Host : $SUB_DOMAIN"
 echo $SUB_DOMAIN > /root/domain
-mkdir /etc/xray/
 cp /root/domain /etc/xray/
 echo $SUB_DOMAIN > /etc/xray/domain
 echo $SUB_DOMAIN > /root/domain
