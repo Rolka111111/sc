02458396d00981a3d81f475171d490cb79bef4ed
Makhluk <126827314+Annnjayy@users.noreply.github.com>
2023-03-30 06:17:18 +0700

    Update restore.sh

--- menu/restore.sh
+++ menu/restore.sh
@@ -1,25 +1,7 @@
 #!/bin/bash
-#clear
-#color
-red='\e[1;31m'
-green='\e[0;32m'
-NC='\e[0m'
-MYIP=$(wget -qO- ipinfo.io/ip);
-echo "Loading..."
-IZIN=$( curl https://raw.githubusercontent.com/Annnjayy/sc/main/name | grep $MYIP )
-if [ $MYIP = $IZIN ]; then
-echo -e "[ ${green}INFO${NC} ] Permission Accepted..."
-else
-echo -e "[ ${green}INFO${red} ] Permission Denied!${NC}";
-echo -e "[ ${green}INFO${NC} ] Please Contact Admin!!"
-echo -e "[ ${green}INFO${NC} ] WhatsApp : 087844547312"
-echo -e "[ ${green}INFO${NC} ] Telegram : https://t.me/MakhlukVpn"
-exit 0
-fi
 clear
 echo "This Feature Can Only Be Used According To Vps Data With This Autoscript"
 echo "Please input link to your vps data backup file."
-echo "You can check it on your email if you run backup data vps before."
 echo -e "===============================" | lolcat
 echo -e "  Mohon Masukan Link Backup  "        
 echo -e "===============================" | lolcat
@@ -45,5 +27,3 @@ echo -e "      Restore Succesfully "
 echo -e "===============================" | lolcat
 echo -e "    Script By MakhlukVpn          "
 echo -e "===============================" | lolcat
-sleep 1
-menu
