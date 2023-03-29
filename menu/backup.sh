ae7a05f8b1a05afd518f318e9b29e214e5e1135e
Makhluk <126827314+Annnjayy@users.noreply.github.com>
2023-03-30 06:19:12 +0700

    Update backup.sh

--- menu/backup.sh
+++ menu/backup.sh
@@ -1,22 +1,5 @@
 #!/bin/bash
 clear
-#color
-red='\e[1;31m'
-green='\e[0;32m'
-NC='\e[0m'
-MYIP=$(wget -qO- ipinfo.io/ip);
-echo "Loading..."
-IZIN=$( curl https://raw.githubusercontent.com/Annnjayy/sc/main/name | grep $MYIP )
-if [ $MYIP = $IZIN ]; then
-echo -e "[ ${green}INFO${NC} ] Permission Accepted..."
-else
-echo -e "[ ${green}INFO${red} ] Permission Denied!${NC}";
-echo -e "[ ${green}INFO${NC} ] Please Contact Admin!!"
-echo -e "[ ${green}INFO${NC} ] WhatsApp : 087844547312"
-echo -e "[ ${green}INFO${NC} ] Telegram : https://t.me/MakhlukVpn"
-exit 0
-fi
-clear
 IP=$(wget -qO- ipinfo.io/ip);
 date=$(date +"%Y-%m-%d")
 domain=$(cat /etc/xray/domain)
@@ -40,10 +23,10 @@ mv backup-$date.zip /home/vps/public_html
 link="https://$domain:89/backup-$date.zip"
 echo -e "The following is a link to your vps data backup file."
 echo -e "===============================" | lolcat
-echo -e "       Detail Backup   "
+echo -e "        Detail Backup   "
 echo -e "===============================" | lolcat
 echo -e " Ip VPS        : $IP"
-echo -e " Domain VPS   : $domain"
+echo -e " Domain VPS    : $domain"
 echo -e " Link Backup   : $link"
 echo -e "===============================" | lolcat
 echo -e "      Script By MakhlukVpn"
