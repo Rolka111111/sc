23e7d838ee862bdc349b56bf1011f05b522460ad
Makhluk <126827314+Annnjayy@users.noreply.github.com>
2023-03-30 05:40:26 +0700

    Update menu.sh

--- menu/menu.sh
+++ menu/menu.sh
@@ -49,19 +49,19 @@ upload=`grep -e "lo:" -e "wlan0:" -e "eth0" /proc/net/dev | awk '{print $10}' | 
 uploadsize=$(($upload/1073741824))
 clear
 echo -e ""
-echo -e "===============================" | lolcat
-echo -e "     ⇱ Sytem Information ⇲           "
-echo -e "===============================" | lolcat
+echo -e "=====================================" | lolcat
+echo -e "        ⇱ Sytem Information ⇲         "
+echo -e "=====================================" | lolcat
 echo -e "• OS Name     : $Tipe , $Versi_OS"
-echo -e "• Cpu Usage    : $cpu_usage"
-echo -e "• Total RAM    : ${totalram}MB"
-echo -e "• Avaible       : ${MEMORY}MB"
-echo -e "• Public IP      : $MYIP"
-echo -e "• Domain       : $Domen"
-echo -e "• ISP Country   : $NAMAISP , $COUNTRY"
-echo -e "===============================" | lolcat
-echo -e "     ⇱ Menu Manager ⇲ "        
-echo -e "===============================" | lolcat
+echo -e "• Cpu Usage   : $cpu_usage"
+echo -e "• Total RAM   : ${totalram}MB"
+echo -e "• Avaible     : ${MEMORY}MB"
+echo -e "• Public IP   : $MYIP"
+echo -e "• Domain      : $Domen"
+echo -e "• ISP Country : $NAMAISP , $COUNTRY"
+echo -e "=====================================" | lolcat
+echo -e "          ⇱ Menu Manager ⇲ "        
+echo -e "=====================================" | lolcat
 echo -e ""
 echo -e " 1}.$green  Manage SSH & OpenVPN"
 echo -e " 2}.$green  Manage X-RAY Vmess & Vless"
@@ -76,19 +76,19 @@ echo -e " 10}.$green Reboot Vps "
 echo -e " x}.$red  Exit"
 echo -e ""
 echo -e "=====================================" | lolcat
-echo -e "• Total Download    : $downloadsize GB "
-echo -e "• Total Upload      : $uploadsize GB "
-echo -e "• Client Name       : $Name"
-echo -e "• Exp Script        : LifeTime"
-echo -e "• Version           : V.1 {latest}"
-echo -e "===============================" | lolcat
-echo -e "   Script By MakhlukVpn          "
-echo -e "===============================" | lolcat
+echo -e "• Total Download : $downloadsize GB "
+echo -e "• Total Upload   : $uploadsize GB "
+echo -e "• Client Name    : $Name"
+echo -e "• Exp Script     : LifeTime"
+echo -e "• Version        : V.1 {latest}"
+echo -e "=====================================" | lolcat
+echo -e "        Script By MakhlukVpn      "
+echo -e "=====================================" | lolcat
 echo -e ""
 echo -e ""
-echo -e "===============================" | lolcat
-read -p "  Please Input Number  [1-10 or [x] ] :  "  menu
-echo -e "===============================" | lolcat
+echo -e "=====================================" | lolcat
+read -p " Please Input Number  [1-10 or [x] ] :  "  menu
+echo -e "=====================================" | lolcat
 case $menu in
 1)
 menu-ssh
