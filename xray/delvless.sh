2f0647bafd94096201f5e1f7c78047cdb8a66e09
Makhluk <126827314+Annnjayy@users.noreply.github.com>
2023-03-30 06:57:10 +0700

    Update delvless.sh

--- xray/delvless.sh
+++ xray/delvless.sh
@@ -5,7 +5,7 @@ MYIP=$(wget -qO- ipinfo.io/ip);
 NUMBER_OF_CLIENTS=$(grep -c -E "^### " "/etc/xray/vless-tls.json")
 	if [[ ${NUMBER_OF_CLIENTS} == '0' ]]; then
 		echo -e "===============================" | lolcat
-		echo -e "     You have no existing clients!"
+		echo -e "  You have no existing clients!"
 		exit 1
 	fi
 
@@ -14,7 +14,7 @@ NUMBER_OF_CLIENTS=$(grep -c -E "^### " "/etc/xray/vless-tls.json")
 	echo " Select the existing client you want to remove"
 	echo " Press CTRL+C to return"
 	echo -e "===============================" | lolcat
-	echo "     No  Expired   User"
+	echo "         No  Expired   User"
 	echo -e "===============================" | lolcat
 	grep -E "^### " "/etc/xray/vless-tls.json" | cut -d ' ' -f 2-3 | nl -s ') '
 	until [[ ${CLIENT_NUMBER} -ge 1 && ${CLIENT_NUMBER} -le ${NUMBER_OF_CLIENTS} ]]; do
@@ -34,11 +34,11 @@ service cron restart
 clear
 echo ""
 echo -e "===============================" | lolcat
-echo -e "   XRAYS/Vless Account Deleted     "
+echo -e "  XRAYS/Vless Account Deleted     "
 echo -e "===============================" | lolcat
 echo -e "  Username  : $user"
 echo -e "  Expired   : $exp"
 echo -e "===============================" | lolcat
-echo -e "    Script By MakhlukVpn          "
+echo -e "      Script By MakhlukVpn          "
 echo -e "===============================" | lolcat
 echo -e ""
