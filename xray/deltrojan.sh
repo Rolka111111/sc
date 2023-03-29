d2471cb1362212cb8771d19f1c16e2b72fea27b4
Makhluk <126827314+Annnjayy@users.noreply.github.com>
2023-03-30 06:54:56 +0700

    Update deltrojan.sh

--- xray/deltrojan.sh
+++ xray/deltrojan.sh
@@ -5,7 +5,7 @@ MYIP=$(wget -qO- ipinfo.io/ip);
 NUMBER_OF_CLIENTS=$(grep -c -E "^#&# " "/etc/xray/trojan.json")
 	if [[ ${NUMBER_OF_CLIENTS} == '0' ]]; then
 		echo -e "================================" | lolcat
-		echo -e "     You have no existing clients!"
+		echo -e "  You have no existing clients!"
 		exit 1
 	fi
 
@@ -13,7 +13,7 @@ NUMBER_OF_CLIENTS=$(grep -c -E "^#&# " "/etc/xray/trojan.json")
 	echo " Select the existing client you want to remove"
 	echo " Press CTRL+C to return"
 	echo " ===============================" | lolcat
-	echo "     No  Expired   User"
+	echo "       No  Expired   User"
 	grep -E "^#&# " "/etc/xray/trojan.json" | cut -d ' ' -f 2-3 | nl -s ') '
 	until [[ ${CLIENT_NUMBER} -ge 1 && ${CLIENT_NUMBER} -le ${NUMBER_OF_CLIENTS} ]]; do
 		if [[ ${CLIENT_NUMBER} == '1' ]]; then
@@ -31,11 +31,11 @@ service cron restart
 clear
 echo ""
 echo -e "================================" | lolcat
-echo -e "    Trojan Account Deleted     "
+echo -e "     Trojan Account Deleted     "
 echo -e "================================" | lolcat
 echo -e " Username  : $user"
 echo -e " Expired   : $exp"
 echo -e "===============================" | lolcat
-echo -e "    Script By MakhlukVpn          "
+echo -e "      Script By MakhlukVpn          "
 echo -e "===============================" | lolcat
 echo -e ""
