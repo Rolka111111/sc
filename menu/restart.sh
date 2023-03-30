96a9b6bf6be211b82483db5d75b57ca43560ee3b
Makhluk <126827314+Annnjayy@users.noreply.github.com>
2023-03-30 20:41:39 +0700

    Update restart.sh

--- menu/restart.sh
+++ menu/restart.sh
@@ -58,8 +58,7 @@ sleep 0.5
 echo -e "[ ${green}INFO${NC} ] Starting Restart SlowDNS... " 
 sleep 0.5
 systemctl restart rc-local > /root/.s/log
-netfilter-persistent reload > /root/.s/log
-screen -dmS badvpn badvpn-udpgw --listen-addr 127.0.0.1:7100 --max-clients 1000
+screen -dmS badvpn badvpn-udpgw --listen-addr 127.0.0.1:7100 --max-clients 1000 
 screen -dmS badvpn badvpn-udpgw --listen-addr 127.0.0.1:7200 --max-clients 1000
 screen -dmS badvpn badvpn-udpgw --listen-addr 127.0.0.1:7300 --max-clients 1000
 echo -e "================================" | lolcat
