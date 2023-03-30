736dc3380fbe44705b0a10e605fd05c0f13b7549
Makhluk <126827314+Annnjayy@users.noreply.github.com>
2023-03-30 07:02:37 +0700

    Update renewvless.sh

--- xray/renewvless.sh
+++ xray/renewvless.sh
@@ -23,12 +23,11 @@ NUMBER_OF_CLIENTS=$(grep -c -E "^#### " "/etc/xray/vless-tls.json")
 	if [[ ${NUMBER_OF_CLIENTS} == '0' ]]; then
 		clear
 		echo -e "================================" | lolcat
-		echo -e "     You have no existing clients!"
+		echo -e "  You have no existing clients!"
 		exit 1
 	fi
 
 	clear
-	echo -e "===============================" | lolcat
 	echo "Select the existing client you want to renew"
 	echo " Press CTRL+C to return"
 	echo -e "===============================" | lolcat
@@ -55,11 +54,11 @@ sed -i "s/#### $user $exp/#### $user $exp4/g" /etc/xray/vless-nontls.json
 clear
 echo ""
 echo -e "===============================" | lolcat
-echo -e "       Vless Account Renewed    "
+echo -e "     Vless Account Renewed    "
 echo -e "===============================" | lolcat
 echo -e "  Username  : $user"
 echo -e "  Expired   : $exp4"
 echo -e "===============================" | lolcat
-echo -e "    Script By MakhlukVpn          "
+echo -e "     Script By MakhlukVpn          "
 echo -e "===============================" | lolcat
 echo -e ""
