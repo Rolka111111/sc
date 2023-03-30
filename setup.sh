7bb67d6a3deb3ef6bf67acb960b1c1563d459266
Makhluk <126827314+Annnjayy@users.noreply.github.com>
2023-03-30 18:54:52 +0700

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -31,7 +31,7 @@ sleep 2
 sysctl -w net.ipv6.conf.all.disable_ipv6=1
 sysctl -w net.ipv6.conf.default.disable_ipv6=1
 #update
-echo -e "[ ${green}INFO${NC} ] Starting Update Package " 
+echo -e "[ ${green}INFO${NC} ] Starting Install Package " 
 sleep 2
 apt update -y
 apt upgrade -y
