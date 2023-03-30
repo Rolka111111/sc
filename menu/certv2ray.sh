7508395ce05bcbe3197ded13b4087d2158b2720d
Makhluk <126827314+Annnjayy@users.noreply.github.com>
2023-03-30 23:33:47 +0700

    Update certv2ray.sh

--- menu/certv2ray.sh
+++ menu/certv2ray.sh
@@ -16,14 +16,38 @@ systemctl enable xray@v2ray-nontls
 systemctl enable xray@vless-tls
 systemctl enable xray@vless-nontls
 systemctl enable xray@trojan
-echo -e "[ ${green}INFO${NC} ] Processing to stop port 80 " 
-sleep 1
+echo -e "[ ${green}INFO${NC} ] Processing 10 second to stop port 80 " 
+sleep 2
+echo -e "1"
 sudo fuser -k 80/tcp
+sleep 0.2
+echo -e "2"
 sudo fuser -k 80/tcp
+sleep 0.2
+echo -e "3"
 sudo fuser -k 80/tcp
+sleep 0.2
+echo -e "4"
 sudo fuser -k 80/tcp
+sleep 0.2
+echo -e "5"
 sudo fuser -k 80/tcp
+sleep 0.2
+echo -e "6"
 sudo fuser -k 80/tcp
+sleep 0.2
+echo -e "7"
+sudo fuser -k 80/tcp
+sleep 0.2
+echo -e "8"
+sudo fuser -k 80/tcp
+sleep 0.2
+echo -e "9"
+sudo fuser -k 80/tcp
+sleep 0.2
+echo -e "10"
+sudo fuser -k 80/tcp
+sleep 0.2
 echo -e "[ ${green}INFO${NC} ] Starting renew cert... " 
 sleep 2
 /root/.acme.sh/acme.sh --issue -d $domain --standalone -k ec-256
